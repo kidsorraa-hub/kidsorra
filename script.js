8a97ca77ac8f8f7b8e7d9df61b1a5022e90eff8c
kidsorraa-hub <kidsorraa@gmail.com>
2026-07-08 10:22:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,36 @@ console.log("Kidsorra button clicked ⭐");
 
 
 });
+
+
+const counters=document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+counter.innerText="0";
+
+const updateCounter=()=>{
+
+const target=+counter.getAttribute("data-target");
+
+const current=+counter.innerText;
+
+const increment=target/120;
+
+if(current<target){
+
+counter.innerText=Math.ceil(current+increment);
+
+setTimeout(updateCounter,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+}
+
+updateCounter();
+
+});
